06f26fa0c093d471a1e06d9fe0247df32835d4cf
NervousBeef7 <34631942+NervousBeef7@users.noreply.github.com>
2017-12-23 00:36:44 +0300

    Add files via upload

---
+++ Новая папка/GrozniMovie/UserWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Movies_Timetabel
+{
+    /// <summary>
+    /// Логика взаимодействия для UserWindow.xaml
+    /// </summary>
+    public partial class UserWindow : Window
+    {
+        public UserWindow()
+        {
+            InitializeComponent();
+
+            List<Movie> movie = Movie.movies;
+
+            listView.ItemsSource = movie;
+
+        }
+
+        private void search_Click(object sender, RoutedEventArgs e)
+        {
+
+            var findMovie = new SearchWindow();
+            findMovie.Show();
+
+        }
+
+        private void main_Click(object sender, RoutedEventArgs e)
+        {
+
+            var main = new MainWindow();
+            this.Close();
+            main.Show();
+
+        }
+    }
+}
